be647fb6b2c0fd6403b0129f1835a1c7b4c7db11
Cayla Franklin <cjfranklin4@gmail.com>
2021-06-10 16:01:49 -0500

    updated api and styling with bootstrap

--- js/main.js
+++ js/main.js
@@ -1,25 +1,32 @@
 document.querySelector('button').addEventListener('click', getPic);
 
+
+//APOD Api
 function getPic (){
     let picDate = document.querySelector('input').value;
     let picImg = document.getElementById('picImg')
     let picVid = document.getElementById('picVid')
-    const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date='+picDate;
+    const url = 'https://api.nasa.gov/planetary/apod?api_key=jQVaJ7ob9ZZdeL4wNdtPyfPAagLeUm37Yb6XzcL1&date='+picDate;
 
     fetch(url)
         .then(res => res.json()) 
         .then(data => {
-            //console.log(data)
+            console.log(data)
             document.querySelector('h2').innerText=data.title;
             
             if(data.media_type === 'image'){
                 picImg.style.display = 'block'
+                picVid.style.display="none"
                 document.querySelector('img').src=data.url
-            } else{
+            } else if (data.media_type === 'video') {
                 picVid.style.display = 'block'
-                document.querySelector('video').src=data.url
+                picImg.style.display='none'
+                document.querySelector('#picVid').src=data.url
+            } else {
+                alert('Please enter a valid date')
+
             }
-           document.querySelector('h3').innerText=data.explanation 
+           document.querySelector('.des').innerText=data.explanation 
 
         })
         .catch(err => {
@@ -27,4 +34,20 @@ function getPic (){
         })
 
     
+}
+
+//Modal Image 
+let modal = document.getElementById("myModal");
+let img = document.getElementById("picImg");
+let modalImg = document.getElementById("img01");
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
 }
